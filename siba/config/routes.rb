35dbc23bd3a556ab5fbafc64ab0612a476c966e0
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-07-28 14:03:43 -0400

    added welcome and bookings

--- siba/config/routes.rb
+++ siba/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+  resources :bookings
+  root to: 'welcome#index'
 end
