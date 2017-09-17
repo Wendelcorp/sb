7885aba24d0f1e0578479c7b54e8c035f22a61be
wendelcorp <bryce@brycewendelaar.com>
2017-09-17 14:28:57 -0400

    Configured more of the form wizard

--- siba/app/controllers/booking_steps_controller.rb
+++ siba/app/controllers/booking_steps_controller.rb
@@ -6,4 +6,14 @@ class BookingStepsController < ApplicationController
     @booking = Booking.find(params[:booking_id])
     render_wizard
   end
+  def update
+    @booking = Booking.find(params[:booking_id])
+    @booking.update(booking_params)
+    render_wizard @booking
+  end
+end
+
+private
+def booking_params
+  params.require(:booking).permit(:details, :name, :email, :phone, :company, :event_type, :date, :bar_type, :add_ons, :address, :guests, :start_time, :permit, :additional, :subtotal)
 end
--- siba/app/controllers/bookings_controller.rb
+++ siba/app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, :except => [:new, :create]
+  # before_action :authenticate_user!, :except => [:new, :create, :update]
   # GET /bookings
   # GET /bookings.json
 
